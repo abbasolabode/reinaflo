0547381b4a87a6fbbbe6f5d14bf69b11f49fd92d
Abbas Olabode <olabodeabbas80@outlook.com>
2026-04-08 19:56:33 +0100

    new fixes

--- src/sections/sectionForAbout/AboutHeroSection.jsx
+++ src/sections/sectionForAbout/AboutHeroSection.jsx
@@ -8,16 +8,14 @@ const bgHeroImage = {
 };
 export default function AboutHeroSection() {
   return (
-    <div style={bgHeroImage} className="px-4 border min-h-screen flex flex-col justify-center items-center gap-10 space-y-9 ">
+    <div style={bgHeroImage} className="px-4 min-h-screen flex flex-col justify-center items-center gap-10 space-y-9 ">
         <div className="flex flex-col gap-3 items-center justify-center">
-            <h2 className='text-xl font-semibold text-blue-600 sm:text-lg md:text-xl lg:text-2xl xl:text-3xl '>Broad Vision. Exceptional Learning. Lasting Impact</h2>
-            <p className='font-light text-center text-white/90'>Our mission, both then and now is to provide quality education while upholding the highest standard of integrity, professionalism and student success.</p>
+            <h2 className='text-xl font-semibold text-white sm:text-lg md:text-xl lg:text-2xl xl:text-3xl '>Broad Vision. Exceptional Learning. Lasting Impact</h2>
+            <p className='font-light text-center text-white'>Our mission, both then and now is to provide quality education while upholding the highest standard of integrity, professionalism and student success.</p>
         </div>
 
-        <h1 className='font-bold text-4xl justify-center items-center text-blue-600 md:text-5xl lg:text-6xl'>About</h1>
+        <h1 className='font-bold text-4xl justify-center items-center text-white md:text-5xl lg:text-6xl'>About</h1>
     </div>
   )
 }
 
-
-// relative z-0 min-h-screen w-full sticky top-0 animate-zoomBlur will-change-[transform,filter]
--- src/sections/sectionForAbout/Story.jsx
+++ src/sections/sectionForAbout/Story.jsx
@@ -11,44 +11,44 @@ const special = [
     {
         id: 1,
         text: "Elite Performance Principles",
-        icon: <IoMdBook size={10} className="w-6 h-6 rounded-full text-white bg-blue-700" />,
+        icon: <IoMdBook size={10} className="w-6 h-6 rounded-full text-white bg-black" />,
     },
     {
         id: 2,
         text: "Leadership Architects",
-        icon: <HiUsers size={10} className="w-6 h-6 rounded-full text-white bg-blue-700" />
+        icon: <HiUsers size={10} className="w-6 h-6 rounded-full text-white bg-black" />
     },
 
     {
         id: 3,
         text: "Human Transformation",
-        icon: <FaHeart size={10} className="w-6 h-6 rounded-full text-white bg-blue-700" />
+        icon: <FaHeart size={10} className="w-6 h-6 rounded-full text-white bg-black" />
     },
     {
         id: 4,
         text: "Global Excellence Standards",
-        icon: <GiAchievement size={10} className="w-6 h-6 rounded-full text-white bg-blue-700" />
+        icon: <GiAchievement size={10} className="w-6 h-6 rounded-full text-white bg-black" />
     },
 ]
 
 const approach = [
     {
         id: 1,
-        icon: <GoDotFill className="w-2 h-2 rounded-full bg-blue-600 shrink-0 mt-2 hover:scale-150 transition-all duration-300 delay-1200" />,
+        icon: <GoDotFill className="w-2 h-2 rounded-full bg-black shrink-0 mt-2 hover:scale-150 transition-all duration-300 delay-1200" />,
         header: "Elite Performance Systems:",
         text: "Structured frameworks inspired by Olympic discipline and high-performance environments",
     },
 
     {
         id: 2,
-        icon: <GoDotFill className="w-2 h-2 rounded-full bg-blue-600 shrink-0 mt-2 hover:scale-150 transition-all duration-300 delay-1200" />,
+        icon: <GoDotFill className="w-2 h-2 rounded-full bg-black shrink-0 mt-2 hover:scale-150 transition-all duration-300 delay-1200" />,
         header: "Leadership Intelligence:",
         text: "Development of strategic thinking, decision-making, and adaptive leadership capacity",
     },
 
     {
         id: 3,
-        icon: <GoDotFill className="w-2 h-2 rounded-full bg-blue-600 shrink-0 mt-2 hover:scale-150 transition-all duration-300 delay-1200" />,
+        icon: <GoDotFill className="w-2 h-2 rounded-full bg-black shrink-0 mt-2 hover:scale-150 transition-all duration-300 delay-1200" />,
         header: "Human Transformation:",
         text: "Cultivating resilience, mindset strength, and sustainable personal growth",
     },
@@ -57,22 +57,22 @@ const approach = [
 
 export default function Story() {
     return (
-        <div className="shadow-lg shadow-white min-h-screen z-50 flex flex-col gap-5 space-y-6 pb-6 px-4">
+        <div className="shadow-lg shadow-white min-h-screen flex flex-col gap-5 space-y-6 pb-6 px-4">
             {/* Left container */}
             <section className="flex flex-col gap-16">
                 <div className="flex space-x-6 items-center">
-                    <hr className="bg-linear-to-r from-blue-800 to-blue-800 h-px transition-all duration-300 delay-200 w-12" />
-                    <h4 className="text-sm font-medium text-blue-700"> About Our School</h4>
+                    <hr className="bg-black h-px transition-all duration-300 delay-200 w-12" />
+                    <h4 className="text-sm font-medium text-black"> About Our School</h4>
                 </div>
                 <div className="flex flex-col space-y-4">
                     <h2 className="text-3xl sm:text-4xl md:text-5xl font-light leading-tight  transition-all duration-300 opacity-100 translate-y-0 ">Our Story</h2>
-                    <hr className=" h-1  w-16 bg-linear-to-r from-blue-700 to-blue-800 transition-all duration-300 delay-200" />
+                    <hr className=" h-1  w-16 bg-black transition-all duration-300 delay-200" />
                 </div>
                 <div className="flex flex-col gap-4">
-                    <p className="text-lg leading-relaxed font-light bg-linear-to-r from-neutral-900 via-blue-800 to-neutral-900 bg-clip-text text-transparent transition-all duration-300 delay-700 opacity-100 translate-y-0">
+                    <p className="text-lg leading-relaxed font-light bg-black bg-clip-text text-transparent transition-all duration-300 delay-700 opacity-100 translate-y-0">
                         Reina-Flor Okori Makendengue is a four-time Olympian, strategist, and change architect working at the intersection of <strong>high performance</strong>, <strong>leadership</strong>, and <strong>human development</strong>.
                     </p>
-                    <div className="bg-blue-50/50 rounded p-6 border-l-4 border-blue-700 hover:translate-x-2 hover:border-l-8 opacity-100 translate-y-0">
+                    <div className="bg-blue-50/50 rounded p-6 border-l-4 border-black hover:translate-x-2 hover:border-l-8 opacity-100 translate-y-0">
                         <p className="text-base leading-relaxed text-gray-700">
                             Competing at the highest level in track and field for both France and Equatorial Guinea, she is among the fastest women in French history in the 100m hurdles. Her career—spanning over two decades—reflects discipline, resilience, and the ability to perform under pressure at the highest level.
                         </p>
@@ -80,18 +80,18 @@ export default function Story() {
                 </div>
 
                 <div className="flex flex-col space-y-5 ">
-                    <h3 className="text-xl font-semibold  text-blue-600">What Makes Us Special</h3>
+                    <h3 className="text-xl font-semibold  text-black">What Makes Us Special</h3>
 
                     <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                         {special.map(item => (
-                            <div key={item.id} className="flex items-center gap-2 shrink-0 group:hover:scale-125 group:hover:rotate-180 duration-300 transition-all"> {item.icon} <p className="bg-linear-to-r from-neutral-900 via-blue-800 to-neutral-900 bg-clip-text text-transparent group-hover:from-blue-300 group-hover:to-blue-700 transition-all duration-200 font-medium ">{item.text}</p></div>
+                            <div key={item.id} className="flex items-center gap-2 shrink-0 group:hover:scale-125 group:hover:rotate-180 duration-300 transition-all"> {item.icon} <p className="text-black font-medium ">{item.text}</p></div>
                         ))}
                     </div>
                 </div>
 
 
                 <div className="w-full space-y-4 transition-all duration-300 delay-1000 opacity-100 translate-y-0">
-                    <h4 className="text-lg font-medium text-blue-800">Our Educational Approach</h4>
+                    <h4 className="text-lg font-medium text-black">Our Educational Approach</h4>
                     {approach.map(item => (
                         <p key={item.id} className="flex items-start gap-2 hover:translate-x-2 transition-all duration-200 delay-1100 leading-relaxed">
 
